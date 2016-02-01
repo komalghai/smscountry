d915c4313cfa4953722a9c690a913ccf05d58ac6
komalghai <komal.3ginfo@gmail.com>
2016-02-01 16:10:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+
+<script>alert(1);</script>;
+<?php echo "https://smsappstore.myshopify.com/admin/webhooks.json??access_token=<?php echo $_SESSION['oauth_token'] ?>";
 <script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
 <script>
   
