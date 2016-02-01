076e559840299c53e85c96dcf5ad932a1e2c492f
komalghai <komal.3ginfo@gmail.com>
2016-02-01 17:25:19 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
-
 <?php session_start(); 
 require __DIR__.'/conf.php';
-$oauth_token = shopify\access_token($_GET['shop'], SHOPIFY_APP_API_KEY, SHOPIFY_APP_SHARED_SECRET, $_GET['code']);
+/* $oauth_token = shopify\access_token($_GET['shop'], SHOPIFY_APP_API_KEY, SHOPIFY_APP_SHARED_SECRET, $_GET['code']);
 		$_SESSION['oauth_token'] = $oauth_token;
-		$_SESSION['shop'] = $_GET['shop'];
+		$_SESSION['shop'] = $_GET['shop']; */
 ?>
-<script>alert("<?php echo $_SESSION['oauth_token']; ?>");</script>
-<?php echo "https://smsappstore.myshopify.com/admin/webhooks.json?access_token=".$_SESSION['oauth_token'];?>
+<?php print_r($_GET); ?>
+<?php print_r($_SESSION); ?>
+<?php /* echo "https://smsappstore.myshopify.com/admin/webhooks.json?access_token=".$_SESSION['oauth_token']; */?>
 <script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
 <script>
   
@@ -24,7 +24,6 @@ $.ajax({
 success: function(response){
   alert('yes');
 }
-
 });
   
 </script>
