3c26f99eb40bf55996340d2f517b7520933b5291
komalghai <komal.3ginfo@gmail.com>
2016-02-01 12:30:24 +0530

    Update conf.php

--- conf.php
+++ conf.php
@@ -2,3 +2,8 @@
 
 	define('SHOPIFY_APP_API_KEY', '2f32ee6aa7b785c18281b4cf8fc26346');
 	define('SHOPIFY_APP_SHARED_SECRET', '8e7e97c6b762aed851b3387f5a249844');
+	  /* DB LOGIN INFORMATION */
+	    define('DB_HOST', 'ec2-54-197-247-170.compute-1.amazonaws.com');
+	    define('DB_USER', 'vrjlpasdpeblfz');
+	    define('DB_PASSWORD', 'N3mICKo5DkmsMjntro7bSPoQQA');
+	    define('DB_NAME', 'daovncsi96sin3');
