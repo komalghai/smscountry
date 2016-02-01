4afbd5a8910f181c8aedfd2da319a90eb9488769
komalghai <komal.3ginfo@gmail.com>
2016-02-01 13:54:05 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
-<?php 
-echo "sdaf";
-?>
+<script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
+<script>
+  
+$.ajax({
+	type: 'POST',
+	url: '/admin/webhooks.json',  
+     data: {
+  "webhook": {
+    "topic": "orders\/create",
+    "address": "https:\/\/smscountry.herokuapp.com\/",
+    "format": "json"
+  }
+},
+success: function(response){
+  alert('yes');
+}
+
+});
+  
+</script>
