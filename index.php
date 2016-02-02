834ab5ffd3ba8a430cd7a483d22344f3a54eb260
komalghai <komal.3ginfo@gmail.com>
2016-02-02 13:06:29 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+	if(!session_id()) session_start(); 
+	require __DIR__.'/vendor/autoload.php';
+	use phpish\shopify;
+	require __DIR__.'/conf.php';
+	isset($_REQUEST['shop']) or die ('Query parameter "shop" missing.');
+	preg_match('/^[a-zA-Z0-9\-]+.myshopify.com$/', $_REQUEST['shop']) or die('Invalid myshopify.com store URL.');
+	$oauth_token = shopify\access_token($_REQUEST['shop'], SHOPIFY_APP_API_KEY, SHOPIFY_APP_SHARED_SECRET, $_REQUEST['code']);
+	$_SESSION['oauth_token'] = $oauth_token;
+	$_SESSION['shop'] = $_REQUEST['shop']; 
+	
+	$data = array(
+			'access_token' => $oauth_token,
+			'webhook' => array(
+				'topic' => 'orders/create',
+				'address' => 'https://smscountry.herokuapp.com/',
+				'format' => 'json',
+			)
+		);
+	$data_string = json_encode($data);																		 
+	$ch = curl_init("https://smsappstore.myshopify.com/admin/webhooks.json");
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+		'Content-Type: application/json',                                                                                
+		'Content-Length: ' . strlen($data_string))                                                                       
+	);
+	$result = curl_exec($ch);
+	echo "<script>windw.location = 'https://smsappstore.myshopify.com/admin/apps';</script>";
+	exit();
+?>
