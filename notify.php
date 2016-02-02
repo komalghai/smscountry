dd2305877340244de7c7136cba957e7c4ec39ac0
komalghai <komal.3ginfo@gmail.com>
2016-02-02 17:48:52 +0530

    Update notify.php

--- notify.php
+++ notify.php
@@ -1,5 +1,13 @@
-<?php 
-echo "<pre>";
-print_r($_REQUEST);
-die;
-?>
+<?php
+$to = "prashant.3ginfo@gmail.com";
+$subject = "HTML email";
+$msg = "An order has been placed!";
+$msg .= "<pre>";
+$msg .= var_dump($_REQUEST);
+$msg .= "</pre>";
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+$headers .= 'From: <webmaster@example.com>' . "\r\n";
+
+mail($to,$subject,$msg,$headers);
+?> 
