6b536367271b2c297e4a841236f82a9223ce7ea0
komalghai <komal.3ginfo@gmail.com>
2016-02-04 14:05:04 +0530

    Update notify.php

--- notify.php
+++ notify.php
@@ -2,12 +2,13 @@
 require_once('php-mailer/class.phpmailer.php');
 $address = "prashant.3ginfo@gmail.com";
 $mail = new PHPMailer();
-$mail->IsSMTP();                          
-$mail->SMTPAuth   = true;                 
-$mail->Host       = "smtp.google.com";
-$mail->Port       = 443;                   
-$mail->Username   = "prashant.3ginfo@gmail.com";
-$mail->Password   = "53*63@83%32#weed";
+$mail->IsSMTP();
+$mail->SMTPAuth = true;
+$mail->Host = "smtp.google.com";
+$mail->Username = "prashant.3ginfo@gmail.com";
+$mail->Password = "53*63@83%32#weed";
+$mail->SMTPSecure = 'tls';
+$mail->Port = 587;
 /* $body = file_get_contents('https://lh3.googleusercontent.com/-6TFgPgFH3lI/AAAAAAAAAAI/AAAAAAAAAAA/3KU8k8aPhKg/mo/photo.jpg?sz=46'); */
 $body = 'testing shopify';
 $mail->SetFrom('webmaster@webandweb.in', 'First Last');
