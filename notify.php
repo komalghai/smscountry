fe01ce4280006b294d95dc35bd4a36bbd29cc719
komalghai <komal.3ginfo@gmail.com>
2016-02-04 13:45:48 +0530

    Update notify.php

--- notify.php
+++ notify.php
@@ -1,19 +1,18 @@
 <?php 
-$to = "prashant.3ginfo@gmail.com";
-$subject = "HTML email";
-$msg = "An order has been placed!";
-/* $msg .= "<pre>";
-$msg .= var_dump($_REQUEST);
-$msg .= "</pre>"; */
-$headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-$headers .= 'From: <webmaster@example.com>' . "\r\n";
-
-if(mail($to,$subject,$msg,$headers)){
-    die("Mail sent to: {$to}");
+require_once('php-mailer/class.phpmailer.php');
+$address = "prashant.3ginfo@gmail.com";
+$mail = new PHPMailer();
+$body = file_get_contents('https://lh3.googleusercontent.com/-6TFgPgFH3lI/AAAAAAAAAAI/AAAAAAAAAAA/3KU8k8aPhKg/mo/photo.jpg?sz=46');
+$body = eregi_replace("[\]",'',$body);
+$mail->SetFrom('webmaster@webandweb.in', 'First Last');
+$mail->AddAddress($address, "Developer");
+$mail->Subject = "PHPMailer test on heroku server!!";
+$mail->AltBody = "To view the message, please use an HTML compatible email viewer!";
+$mail->MsgHTML($body);
+if(!$mail->Send()) {
+	echo "Mailer Error: " . $mail->ErrorInfo;
 } else {
-	echo "<pre>";
-	print_r(error_get_last());
-    die("Mail not sent!");
+	echo "Message sent!";
 }
-?> 
+die;
+?>
