de6c42f7a1ddbc89ff11bc0e9b3f5f9232c96586
komalghai <komal.3ginfo@gmail.com>
2016-02-05 17:52:19 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,35 +1,35 @@
 <?php
-$user="shopifyplugin";
-$password="Shopify4plug"; 
-$mobilenumbers="918146105676"; 
-$message = "test messgae";
-$senderid="SMSCNTRY";
-$messagetype="N";
-$DReports="Y";
-$url="http://www.smscountry.com/SMSCwebservice_Bulk.aspx";
-$message = urlencode($message);
-$ch = curl_init();
-if (!$ch){
-	die("Couldn't initialize a cURL handle");
-}
-$ret = curl_setopt($ch, CURLOPT_URL,$url);
-curl_setopt ($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-curl_setopt ($ch, CURLOPT_POSTFIELDS,
-"User=$user&passwd=$password&mobilenumber=$mobilenumbers&message=$message&sid=$senderid&mtype=$messagetype&DR=$DReports");
-$ret = curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-$curlresponse = curl_exec($ch);
-if(curl_errno($ch))
-	echo 'curl error : '. curl_error($ch);
-if (empty($ret)) {
-	die(curl_error($ch));
-	curl_close($ch);
-} else {
-	$info = curl_getinfo($ch);
-	curl_close($ch);
-	echo $curlresponse;
-	echo "Message Sent Succesfully" ;
-}
-exit('199');
+	if(!session_id()) session_start(); 
+	require __DIR__.'/vendor/autoload.php';
+	use phpish\shopify;
+	require __DIR__.'/conf.php';
+	isset($_REQUEST['shop']) or die ('Query parameter "shop" missing.');
+	preg_match('/^[a-zA-Z0-9\-]+.myshopify.com$/', $_REQUEST['shop']) or die('Invalid myshopify.com store URL.');
+	$oauth_token = shopify\access_token($_REQUEST['shop'], SHOPIFY_APP_API_KEY, SHOPIFY_APP_SHARED_SECRET, $_REQUEST['code']);
+	$_SESSION['oauth_token'] = $oauth_token;
+	$_SESSION['shop'] = $_REQUEST['shop']; 
+	
+	$data = array(
+			'access_token' => $oauth_token,
+			'webhook' => array(
+				'address' => 'https://smscountry.herokuapp.com/notify.php',
+				'format' => 'json',
+				'topic' => 'orders/create',
+			)
+		);
+	$data_string = json_encode($data);																		 
+	$ch = curl_init("https://smsappstore.myshopify.com/admin/webhooks.json");
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+		'Content-Type: application/json',                                                                                
+		'Content-Length: ' . strlen($data_string))                                                                       
+	);
+	$response = curl_exec($ch);
+	echo "<pre>";
+	print_r(json_decode($response));
+	die;
+	echo "<script>window.location = 'https://smsappstore.myshopify.com/admin/apps';</script>";
+	exit();
 ?>
