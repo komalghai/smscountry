ce24d85f4beafdc90985dce1da715ba651d07f8d
komalghai <komal.3ginfo@gmail.com>
2016-02-05 13:40:37 +0530

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,6 @@ $_SESSION['shop'] = $_REQUEST['shop'];
 <script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
 <script>
 $.ajax({
-	type: 'post',
 	url: "https://smsappstore.myshopify.com/admin/webhooks.json",  
 	dataType:'json',
 	data:{
