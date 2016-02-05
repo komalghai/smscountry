8096a9a2e8d932d2ff5b4684eced156e6936c0f1
komalghai <komal.3ginfo@gmail.com>
2016-02-05 17:44:38 +0530

    Update notify.php

--- notify.php
+++ notify.php
@@ -5,8 +5,8 @@ $mail = new PHPMailer();
 $mail->IsSMTP();
 $mail->SMTPAuth = true;
 $mail->Host = "ssl://smtp.gmail.com";
-$mail->Username = "prashant.3ginfo@gmail.com";
-$mail->Password = "53*63@83%32#weed";
+$mail->Username = "test@gmail.com";
+$mail->Password = "test@gmail.com";
 $mail->SMTPSecure = 'tls';
 $mail->Port = 587;
 /* $body = file_get_contents('https://lh3.googleusercontent.com/-6TFgPgFH3lI/AAAAAAAAAAI/AAAAAAAAAAA/3KU8k8aPhKg/mo/photo.jpg?sz=46'); */
