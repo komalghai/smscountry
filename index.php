b84e7ae51bea3dcab8c234c4ec340af3a33cb2cc
komalghai <komal.3ginfo@gmail.com>
2016-02-05 13:05:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php
+<?
 
     session_start();
 
@@ -24,7 +24,7 @@ $response = curl_exec($ch) */
  $ch = curl_init();
 
     curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_POST, 0);
+	curl_setopt($ch, CURLOPT_HTTPGET, 1);
     curl_setopt($ch, CURLOPT_POSTFIELDS,$query);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
