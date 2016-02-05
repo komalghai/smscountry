993653c7d56a894240f9998eee39b91267f84b4b
komalghai <komal.3ginfo@gmail.com>
2016-02-05 17:27:29 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,33 +1,35 @@
-<?php 
-if(!session_id()) session_start(); 
-require __DIR__.'/vendor/autoload.php';
-use phpish\shopify;
-require __DIR__.'/conf.php';
-isset($_REQUEST['shop']) or die ('Query parameter "shop" missing.');
-preg_match('/^[a-zA-Z0-9\-]+.myshopify.com$/', $_REQUEST['shop']) or die('Invalid myshopify.com store URL.');
-$oauth_token = shopify\access_token($_REQUEST['shop'], SHOPIFY_APP_API_KEY, SHOPIFY_APP_SHARED_SECRET, $_REQUEST['code']);
-$_SESSION['oauth_token'] = $oauth_token;
-$_SESSION['shop'] = $_REQUEST['shop'];	 
+<?php
+$user="shopifyplugin";
+$password="Shopify4plug"; 
+$mobilenumbers="919464549437"; 
+$message = "test messgae";
+$senderid="SMSCNTRY";
+$messagetype="N";
+$DReports="Y";
+$url="http://www.smscountry.com/SMSCwebservice_Bulk.aspx";
+$message = urlencode($message);
+$ch = curl_init();
+if (!$ch){
+	die("Couldn't initialize a cURL handle");
+}
+$ret = curl_setopt($ch, CURLOPT_URL,$url);
+curl_setopt ($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+curl_setopt ($ch, CURLOPT_POSTFIELDS,
+"User=$user&passwd=$password&mobilenumber=$mobilenumbers&message=$message&sid=$senderid&mtype=$messagetype&DR=$DReports");
+$ret = curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$curlresponse = curl_exec($ch);
+if(curl_errno($ch))
+	echo 'curl error : '. curl_error($ch);
+if (empty($ret)) {
+	die(curl_error($ch));
+	curl_close($ch);
+} else {
+	$info = curl_getinfo($ch);
+	curl_close($ch);
+	echo $curlresponse;
+	echo "Message Sent Succesfully" ;
+}
+exit('199');
 ?>
-<script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
-<script>
-/* $.ajax({
-	url: "https://smsappstore.myshopify.com/admin/webhooks.json",  
-	dataType:'json',
-	headers: {"Access-Control-Allow-Origin": "*"},
-	data:{
-		access_token: '<?php echo $oauth_token; ?>',
-	},
-	success: function(response){
-		alert(response);
-	}
-}); */
-
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://smsappstore.myshopify.com/admin/webhooks.json?access_token=<?php echo $oauth_token; ?>", true);
-xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
-xhr.onload = function () {
-    console.log(xhr.responseText);
-};
-xhr.send();
-</script>
