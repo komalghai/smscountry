af51eaa0570c4e7b31a757908f4b5161142db01b
komalghai <komal.3ginfo@gmail.com>
2016-02-05 13:29:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,13 @@
-<?php  session_start();
-    require __DIR__.'/vendor/autoload.php';
-    use phpish\shopify;
-    require __DIR__.'/conf.php';
-  $oauth_token = shopify\access_token($_GET['shop'], SHOPIFY_APP_API_KEY, SHOPIFY_APP_SHARED_SECRET, $_GET['code']);
-	$_SESSION['oauth_token'] = $oauth_token;
-		$_SESSION['shop'] = $_GET['shop']; 
-		echo "hello";?>
-		<script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
-<script>
-  
-$.ajax({
-	type: 'GET',
-	 crossOrigin: true,
-	url: "https://smsappstore.myshopify.com/admin/webhooks.json?access_token=<?php echo $oauth_token; ?>",  
-	dataType:'json',
-	success: function(response){
-	alert('yes');
-	}
-});
-  
-</script>
+<?php 																	 
+	$ch = curl_init("https://smsappstore.myshopify.com/admin/webhooks.json");                                              
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+		'Content-Type: application/json',                                                                   
+	);
+	$response = curl_exec($ch);
+	echo "<pre>";
+	print_r(json_decode($response));
+	die;
+	echo "<script>window.location = 'https://smsappstore.myshopify.com/admin/apps';</script>";
+	exit();
+?>
