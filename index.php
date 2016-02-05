ad42c0e84bc53e134f6a7d5ad6cbca73a8848928
komalghai <komal.3ginfo@gmail.com>
2016-02-05 12:06:37 +0530

    Update index.php

--- index.php
+++ index.php
@@ -10,10 +10,30 @@
   $oauth_token = shopify\access_token($_GET['shop'], SHOPIFY_APP_API_KEY, SHOPIFY_APP_SHARED_SECRET, $_GET['code']);
 	$_SESSION['oauth_token'] = $oauth_token;
 		$_SESSION['shop'] = $_GET['shop']; 
-		echo "hello";
-  $shopify = shopify\client($_GET['shop'], SHOPIFY_APP_API_KEY, SHOPIFY_APP_SHARED_SECRET, true);
-  print_r($shopify);
-  /* 
+		echo "hello";?>
+		<script>
+  
+$.ajax({
+	type: 'GET',
+	url: "https://smsappstore.myshopify.com/admin/webhooks.json?access_token=<?php echo $oauth_token; ?>",  
+	dataType:'json',
+     data: {
+  "webhook": {
+    "topic": "orders/create",
+    "address": "https://smscountry.herokuapp.com/",
+    "format": "json"
+  }
+},
+success: function(response){
+  alert('yes');
+}
+});
+  
+</script>
+
+<?php 
+  /*   $shopify = shopify\client($_GET['shop'], SHOPIFY_APP_API_KEY, SHOPIFY_APP_SHARED_SECRET, true);
+
     try
     {
         # Making an API request can throw an exception
