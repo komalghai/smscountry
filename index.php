5d483f9d0f8fe82588b69b0b6da9db5074d4cb30
komalghai <komal.3ginfo@gmail.com>
2016-02-05 13:13:23 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
-<?
-
-    session_start();
-
+<?php  session_start();
     require __DIR__.'/vendor/autoload.php';
     use phpish\shopify;
-
     require __DIR__.'/conf.php';
-
   $oauth_token = shopify\access_token($_GET['shop'], SHOPIFY_APP_API_KEY, SHOPIFY_APP_SHARED_SECRET, $_GET['code']);
 	$_SESSION['oauth_token'] = $oauth_token;
 		$_SESSION['shop'] = $_GET['shop']; 
@@ -17,7 +12,7 @@
 $.ajax({
 	type: 'GET',
 	url: "https://smsappstore.myshopify.com/admin/webhooks.json?access_token=<?php echo $oauth_token; ?>",  
-	dataType:'json'
+	dataType:'json',
 	success: function(response){
 	alert('yes');
 	}
