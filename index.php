e7b8cd8a0dd3bced377b6e75dfd14c7773563fa4
komalghai <komal.3ginfo@gmail.com>
2016-02-05 13:11:13 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php
+<?
 
     session_start();
 
@@ -10,64 +10,17 @@
   $oauth_token = shopify\access_token($_GET['shop'], SHOPIFY_APP_API_KEY, SHOPIFY_APP_SHARED_SECRET, $_GET['code']);
 	$_SESSION['oauth_token'] = $oauth_token;
 		$_SESSION['shop'] = $_GET['shop']; 
-		echo "hello";
-$url = "https://smsappstore.myshopify.com/admin/webhooks.json";
-$query="access_token=$_SESSION['oauth_token']";
-/* $ch = curl_init($url);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");                                                     
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
-	'Content-Type: application/json',                                                                                
-	'X-Shopify-Access-Token: '.$oauth_token,
-);
-$response = curl_exec($ch) */
- $ch = curl_init();
-
-    curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_HTTPGET, 1);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
-	'Content-Type: application/json'                                                                               
-);
-    $return = curl_exec ($ch);
-    curl_close ($ch);
-
-    echo $return;
-
-  /* 
-echo "<pre>";
-print_r($response);
-  $shopify = shopify\client($_GET['shop'], SHOPIFY_APP_API_KEY, SHOPIFY_APP_SHARED_SECRET, true);
-
-    try
-    {
-        # Making an API request can throw an exception
-        $customers = $shopify('POST /admin/webhooks.json?access_token='.<?php echo $_SESSION['oauth_token'] ?>, array(), array
-        (
-            'webook' => array 
-            (
-                "topic": "customers/create",
-                "address": "https://smscountry.herokuapp.com/",
-                "format": "json"
-            )
-
-        ));
-
-        print_r($customers);
-    }
-    catch (shopify\ApiException $e)
-    {
-        # HTTP status code was >= 400 or response contained the key 'errors'
-        echo $e;
-        print_R($e->getRequest());
-        print_R($e->getResponse());
-    }
-    catch (shopify\CurlException $e)
-    {
-        # cURL error
-        echo $e;
-        print_R($e->getRequest());
-        print_R($e->getResponse());
-    }
- */
-?>
+		echo "hello";?>
+		<script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
+<script>
+  
+$.ajax({
+	type: 'GET',
+	url: "https://smsappstore.myshopify.com/admin/webhooks.json?access_token=<?php echo $oauth_token; ?>",  
+	dataType:'json'
+	success: function(response){
+	alert('yes');
+	}
+});
+  
+</script>
