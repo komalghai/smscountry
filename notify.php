39c8f153dbf139a8935b257f7cf4964409ffcc02
komalghai <komal.3ginfo@gmail.com>
2016-02-06 14:58:06 +0530

    Update notify.php

--- notify.php
+++ notify.php
@@ -2,6 +2,18 @@
 require('conf.php');
 if(!session_id()) session_start();
 global $db;
+
+$result = pg_query($db, "SELECT * FROM debug WHERE key = 'data'");
+if(pg_num_rows($result)) 
+{
+   while($response = pg_fetch_assoc($result)) {
+		echo "<pre>";
+		print_R($response);
+		echo "</pre>";
+	}
+}
+exit('testing');
+
 date_set_timezone_default('Asia/Kolkata');
 $now = date('Y-m-d H:i:s');
 $data = "updated {$now}\n\n";
