fe52d26c7289f3c1a0135efd603d565a8ae67471
komalghai <komal.3ginfo@gmail.com>
2016-02-06 13:18:23 +0530

    Update conf.php

--- conf.php
+++ conf.php
@@ -1,9 +1,13 @@
-<?php
+<?php 
+define('SHOPIFY_APP_API_KEY', '7662e5daf152854818a5066e4d5e61d6'); 
+define('SHOPIFY_APP_SHARED_SECRET', '77e2b001dfdb274bada219d669a0528d');
 
-	define('SHOPIFY_APP_API_KEY', '2f32ee6aa7b785c18281b4cf8fc26346');
-	define('SHOPIFY_APP_SHARED_SECRET', '8e7e97c6b762aed851b3387f5a249844');
-	  /* DB LOGIN INFORMATION */
-	    define('DB_HOST', 'ec2-54-197-247-170.compute-1.amazonaws.com');
-	    define('DB_USER', 'vrjlpasdpeblfz');
-	    define('DB_PASSWORD', 'N3mICKo5DkmsMjntro7bSPoQQA');
-	    define('DB_NAME', 'daovncsi96sin3');
+$host = "host=ec2-54-197-247-170.compute-1.amazonaws.com"; 
+$port = "port=5432"; 
+$dbname = "dbname=daovncsi96sin3"; 
+$credentials = "user=vrjlpasdpeblfz password=N3mICKo5DkmsMjntro7bSPoQQA"; 
+$db = pg_connect( "$host $port $dbname $credentials" ); 
+if(!$db){ 
+	echo "Error : Unable to open database\n"; 
+} 
+?>
