f635efa7041fb1b34f14735adfbe17d686ab579e
komalghai <komal.3ginfo@gmail.com>
2016-02-08 16:45:10 +0530

    Update conf.php

--- conf.php
+++ conf.php
@@ -1,5 +1,5 @@
 <?php 
-define('SHOPIFY_APP_API_KEY', '7662e5daf152854818a5066e4d5e61d6'); 
+define('SHOPIFY_APP_API_KEY', '2f32ee6aa7b785c18281b4cf8fc26346'); 
 define('SHOPIFY_APP_SHARED_SECRET', '8e7e97c6b762aed851b3387f5a249844');
 
 $host = "host=ec2-54-197-247-170.compute-1.amazonaws.com"; 
