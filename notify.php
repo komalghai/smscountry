970553727d2a778bcaa5d93c2c632f3d573bed87
komalghai <komal.3ginfo@gmail.com>
2016-02-08 16:56:54 +0530

    Update notify.php

--- notify.php
+++ notify.php
@@ -4,8 +4,8 @@ if(!session_id()) session_start();
 global $db;
 date_set_timezone_default('Asia/Kolkata');
 $now = date('Y-m-d H:i:s');
-$data = "updated {$now}\n\n";
-$data .= '=====================REQUEST START ============================\n';
+$data = "Updated: {$now}";
+/*$data .= '=====================REQUEST START ============================\n';
 $data .= print_r($_REQUEST, true);
 $data .= '=====================REQUEST END ============================\n\n';
 $data .= '=====================SERVER START ============================\n';
@@ -16,7 +16,7 @@ $data .= print_r($_SESSION, true);
 $data .= '=====================SESSION END ============================\n';
 $data .= '=====================COOKIE START ============================\n';
 $data .= print_r($_COOKIE, true);
-$data .= '=====================COOKIE END ============================\n';
+$data .= '=====================COOKIE END ============================\n'; */
 pg_query($db, "UPDATE debug SET value = '{$data}' WHERE key = 'data'");
 
 $result = pg_query($db, "SELECT * FROM debug WHERE key = 'data'");
