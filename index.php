5679d7de60d2feca921cee0ba04c5d2cdfc3ee85
komalghai <komal.3ginfo@gmail.com>
2016-02-09 15:57:59 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,13 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 if(isset($_REQUEST['code']) && isset($_REQUEST['shop']) && !empty($_REQUEST['code'])){
-	require __DIR__.'/vendor/autoload.php';
-	use phpish\shopify;
 	require __DIR__.'/conf.php';
-	$access_token = shopify\access_token($_REQUEST['shop'], SHOPIFY_APP_API_KEY, SHOPIFY_APP_SHARED_SECRET, $_REQUEST['code']);
-	$_SESSION['oauth_token'] = $access_token;
-	$_SESSION['shop'] = $_REQUEST['shop']; 
-																	 
 	$url = "https://smsappstore.myshopify.com/admin/webhooks.json";
 	$topics = array(
 			'customers/create' => 'https://smscountry.herokuapp.com/notify.php?action=customer_signup',
@@ -17,7 +11,6 @@ if(isset($_REQUEST['code']) && isset($_REQUEST['shop']) && !empty($_REQUEST['cod
 		);
 	foreach($topics as $topic => $address){
 		$data = array(
-				'access_token' => $access_token,
 				'webhook' => array(
 					'address' => $address,
 					'format' => 'json',
