bbfce36a44b17b73e73058428a902d00efc91f6f
komalghai <komal.3ginfo@gmail.com>
2016-02-09 15:39:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php 
 if(isset($_REQUEST['code']) && isset($_REQUEST['shop']) && !empty($_REQUEST['code'])){
+	require __DIR__.'/vendor/autoload.php';
+	use phpish\shopify;
+	require __DIR__.'/conf.php';
 	$access_token = shopify\access_token($_REQUEST['shop'], SHOPIFY_APP_API_KEY, SHOPIFY_APP_SHARED_SECRET, $_REQUEST['code']);
 	$_SESSION['oauth_token'] = $access_token;
 	$_SESSION['shop'] = $_REQUEST['shop']; 
