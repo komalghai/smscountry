2e2717e6fc3ecf87c0d8eda46f0297d96421c898
komalghai <komal.3ginfo@gmail.com>
2016-02-09 16:19:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
 if(isset($_REQUEST['code']) && isset($_REQUEST['shop']) && !empty($_REQUEST['code'])){
 	require __DIR__.'/conf.php';
+	require __DIR__.'/vendor/autoload.php';
+	use phpish\shopify;
+	$access_token = shopify\access_token($_REQUEST['shop'], SHOPIFY_APP_API_KEY, SHOPIFY_APP_SHARED_SECRET, $_REQUEST['code']);
 	$url = "https://smsappstore.myshopify.com/admin/webhooks.json";
 	$topics = array(
 			'customers/create' => 'https://smscountry.herokuapp.com/notify.php?action=customer_signup',
@@ -11,6 +12,7 @@ if(isset($_REQUEST['code']) && isset($_REQUEST['shop']) && !empty($_REQUEST['cod
 		);
 	foreach($topics as $topic => $address){
 		$data = array(
+				'access_token' => $access_token,
 				'webhook' => array(
 					'address' => $address,
 					'format' => 'json',
@@ -19,7 +21,7 @@ if(isset($_REQUEST['code']) && isset($_REQUEST['shop']) && !empty($_REQUEST['cod
 			);
 		$data = json_encode($data);	
 		$ch = curl_init($url);
-		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');                                                                     
 		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);                                                                  
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
 		curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
