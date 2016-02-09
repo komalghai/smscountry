d95e1ec754cd98ddd4d99db8aa4851ee183aeb88
komalghai <komal.3ginfo@gmail.com>
2016-02-09 15:11:53 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,6 @@
 <?php 
-// dev
+if(isset($_REQUEST['code']) && isset($_REQUEST['shop']) && !empty($_REQUEST['code'])){
+	echo "<script>window.location = 'https://smsappstore.myshopify.com/admin/apps';</script>";
+	exit();
+}
+?>
