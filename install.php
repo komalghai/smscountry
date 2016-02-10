fab2b09e9959d2b3eb1f6c54f8d0432957253cd7
komalghai <komal.3ginfo@gmail.com>
2016-02-10 12:22:27 +0530

    Update install.php

--- install.php
+++ install.php
@@ -3,8 +3,9 @@
 	use phpish\shopify;
 	require __DIR__.'/conf.php';
 	isset($_REQUEST['shop']) or die ('Query parameter "shop" missing.');
+	echo "<script>alert($_REQUEST['shop'])</script>";
 	preg_match('/^[a-zA-Z0-9\-]+.myshopify.com$/', $_REQUEST['shop']) or die('Invalid myshopify.com store URL.');
-echo 	$install_url = shopify\install_url($_REQUEST['shop'], SHOPIFY_APP_API_KEY);
-	/*echo "<script>window.location='{$install_url}';</script>";*/
+	$install_url = shopify\install_url($_REQUEST['shop'], SHOPIFY_APP_API_KEY);
+	echo "<script>window.location='{$install_url}';</script>";
 	exit();
 ?>
