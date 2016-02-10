9b28845a35d246804f7f1a3b7c52531eaeb39c2d
komalghai <komal.3ginfo@gmail.com>
2016-02-10 19:59:46 +0530

    Update index.php

--- index.php
+++ index.php
@@ -50,27 +50,56 @@ if(pg_num_rows($shop_exists) < 1){
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Settings - SMS Country</title>
+		<title>Configuration - SMS Country</title>
 		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 	</head>
 	<body>
-		<div class="col-xs-12">
-			<div class="container well">
-				<?php if(isset($_GET['conf']) && ($_GET['conf'] == 200)){ ?>
-					<div class="alert alert-fade alert-success">App installed successfully!</div>
-				<?php } ?>
-				<ul class="tabs">
-					<li><a href="#tab-1">Customer SMS Alerts</a></li>
-					<li><a href="#tab-2">Admin SMS Alerts</a></li>
-				</ul>
-				<div id="tab-1">
-					content 1
+		<div class="col-xs-12 padding-top sms-config">
+			<h3 class="alert alert-info">Configuration</h3>
+			<form>
+				<div class="box container well col-xs-12" style="padding: 0">
+					<ul class="tabs nav">
+						<li class="col-xs-6 active"><a href="#customer-sms-alerts">Customer SMS Alerts</a></li>
+						<li class="col-xs-6"><a href="#admin-sms-alerts">Admin SMS Alerts</a></li>
+					</ul>
+					<div id="customer-sms-alerts" class="nav-content" style="display: block;">
+						<div class="col-xs-4">
+							<h4>New Customer Signup:</h4>
+							<code>
+								You can use following variables: [shop_name], [shop_domain], [customer_firstname], [customer_lastname].
+							</code>
+						</div>
+						<div class="col-xs-6">
+							<div class="form-group">
+								<label for="email"></label>
+								<input type="email" class="form-control" id="email">
+							</div>
+						</div>
+						<div class="col-xs-2">
+							<button class="btn btn-info">Send Test SMS</button>
+						</div>
+					</div>
+					<div id="admin-sms-alerts" class="nav-content">
+						<div class="col-xs-4">
+							<h4>New Customer Signup:</h4>
+							<code>
+								You can use following variables: [shop_name], [shop_domain], [customer_firstname], [customer_lastname].
+							</code>
+						</div>
+						<div class="col-xs-6">
+							<div class="form-group">
+								<label for="email"></label>
+								<input type="email" class="form-control" id="email">
+							</div>
+						</div>
+						<div class="col-xs-2">
+							<button class="btn btn-info">Send Test SMS</button>
+						</div>
+					</div>   
+					<button type="submit" class="btn btn-success">Submit</button>				
 				</div>
-				<div id="tab-2">
-					content 2
-				</div>     
-			</div>
+			</form>
 		</div>
 	</body>
 </html>
