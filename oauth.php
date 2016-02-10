0020fdfef35fbc8d5ed102b705871a5df66806df
komalghai <komal.3ginfo@gmail.com>
2016-02-10 11:26:50 +0530

    Update oauth.php

--- oauth.php
+++ oauth.php
@@ -22,8 +22,35 @@
 		$oauth_token = shopify\access_token($_GET['shop'], SHOPIFY_APP_API_KEY, SHOPIFY_APP_SHARED_SECRET, $_GET['code']);
 		$_SESSION['oauth_token'] = $oauth_token;
 		$_SESSION['shop'] = $_GET['shop'];
-                echo "<script>alert('abc');</script>";
-		echo 'App Successfully Installed!';
+		$access_token = shopify\access_token($_REQUEST['shop'], SHOPIFY_APP_API_KEY, SHOPIFY_APP_SHARED_SECRET, $_REQUEST['code']);
+		$url = "https://smsappstore.myshopify.com/admin/webhooks.json";
+		$topics = array(
+				'customers/create' => 'https://smscountry.herokuapp.com/notify.php?action=customer_signup',
+				'orders/create' => 'https://smscountry.herokuapp.com/notify.php?action=order_created',
+				'orders/updated' => 'https://smscountry.herokuapp.com/notify.php?action=order_updated',
+			);
+		foreach($topics as $topic => $address){
+			$data = array(
+					'access_token' => $access_token,
+					'webhook' => array(
+						'address' => $address,
+						'format' => 'json',
+						'topic' => $topic,
+					)
+				);
+			$data = json_encode($data);	
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');                                                                     
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $data);                                                                  
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
+			curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+				'Content-Type: application/json',                                                                                
+				'Content-Length: ' . strlen($data))                                                                       
+			);
+			curl_exec($ch);
+		}
+		echo "<script>window.location = 'https://smsappstore.myshopify.com/admin/apps/smscountry?conf=200';</script>";
+		exit();
 	}
 	catch (shopify\ApiException $e)
 	{
