5a7f4583280061b968d13a29028327365f16caa7
komalghai <komal.3ginfo@gmail.com>
2016-02-10 11:38:09 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,42 +1,6 @@
 <?php 
-require __DIR__.'/vendor/autoload.php';
-use phpish\shopify;
-echo "<pre>";
-print_r($_GET);
-print_r($_REQUEST);
-die;
-if(isset($_GET['code']) && isset($_GET['shop']) && !empty($_GET['code'])){
-	require __DIR__.'/conf.php';
-	$access_token = shopify\access_token($_REQUEST['shop'], SHOPIFY_APP_API_KEY, SHOPIFY_APP_SHARED_SECRET, $_REQUEST['code']);
-	$url = "https://smsappstore.myshopify.com/admin/webhooks.json";
-	$topics = array(
-			'customers/create' => 'https://smscountry.herokuapp.com/notify.php?action=customer_signup',
-			'orders/create' => 'https://smscountry.herokuapp.com/notify.php?action=order_created',
-			'orders/updated' => 'https://smscountry.herokuapp.com/notify.php?action=order_updated',
-		);
-	foreach($topics as $topic => $address){
-		$data = array(
-				'access_token' => $access_token,
-				'webhook' => array(
-					'address' => $address,
-					'format' => 'json',
-					'topic' => $topic,
-				)
-			);
-		$data = json_encode($data);	
-		$ch = curl_init($url);
-		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');                                                                     
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);                                                                  
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
-		curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
-			'Content-Type: application/json',                                                                                
-			'Content-Length: ' . strlen($data))                                                                       
-		);
-		curl_exec($ch);
-	}
-	echo "<script>window.location = 'https://smsappstore.myshopify.com/admin/apps';</script>";
-	exit();
-}
+require __DIR__.'/conf.php'; 
+global $db;
 ?>
 <!DOCTYPE html>
 <html>
@@ -46,17 +10,22 @@ if(isset($_GET['code']) && isset($_GET['shop']) && !empty($_GET['code'])){
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 	</head>
 	<body>
-		<div class="container well">
-			<ul class="tabs">
-				<li><a href="#tab-1">Customer SMS Alerts</a></li>
-				<li><a href="#tab-2">Admin SMS Alerts</a></li>
-			</ul>
-			<div id="tab-1">
-				content 1
+		<div class="col-xs-12">
+			<div class="container well">
+				<?php if(isset($_GET['conf']) && ($_GET['conf'] == 200)){ ?>
+					<div class="alert alert-fade alert-success">App installed successfully!</div>
+				<?php } ?>
+				<ul class="tabs">
+					<li><a href="#tab-1">Customer SMS Alerts</a></li>
+					<li><a href="#tab-2">Admin SMS Alerts</a></li>
+				</ul>
+				<div id="tab-1">
+					content 1
+				</div>
+				<div id="tab-2">
+					content 2
+				</div>     
 			</div>
-			<div id="tab-2">
-				content 2
-			</div>     
 		</div>
 	</body>
 </html>
