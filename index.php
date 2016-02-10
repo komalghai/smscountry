7deff4e0eb9eec770c5e6953e7db11a35db27af9
komalghai <komal.3ginfo@gmail.com>
2016-02-10 13:09:42 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,39 @@
 <?php 
 require __DIR__.'/conf.php'; 
+if(!session_id()) session_start();
 global $db;
+if(isset($_SESSION['installing']) && $_SESSION['installing'] == true){
+	$access_token = shopify\access_token($_REQUEST['shop'], SHOPIFY_APP_API_KEY, SHOPIFY_APP_SHARED_SECRET, $_REQUEST['code']);
+	$url = "https://smsappstore.myshopify.com/admin/webhooks.json";
+	$topics = array(
+			'customers/create' => 'https://smscountry.herokuapp.com/notify.php?action=customer_signup',
+			'orders/create' => 'https://smscountry.herokuapp.com/notify.php?action=order_created',
+			'orders/updated' => 'https://smscountry.herokuapp.com/notify.php?action=order_updated',
+		);
+	foreach($topics as $topic => $address){
+		$data = array(
+				'access_token' => $access_token,
+				'webhook' => array(
+					'address' => $address,
+					'format' => 'json',
+					'topic' => $topic,
+				)
+			);
+		$data = json_encode($data);	
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');                                                                     
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);                                                                  
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+			'Content-Type: application/json',                                                                                
+			'Content-Length: ' . strlen($data))                                                                       
+		);
+		curl_exec($ch);
+	}
+	unset($_SESSION['installing']);
+	echo "<script>window.location = 'https://smsappstore.myshopify.com/admin/apps/smscountry?conf=200';</script>";
+	exit();
+}
 ?>
 <!DOCTYPE html>
 <html>
