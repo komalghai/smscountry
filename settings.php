e74cb7b865fb18781c76758514a82243d0bf8032
komalghai <komal.3ginfo@gmail.com>
2016-02-11 13:34:59 +0530

    Update settings.php

--- settings.php
+++ settings.php
@@ -3,6 +3,7 @@
 	<head>
 		<title>Configuration - SMS Country</title>
 		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 		<!--script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script-->
 		<script type="text/javascript">
 		jQuery(function(){
