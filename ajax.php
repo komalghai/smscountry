1f05b40c01c7ef95b8c347bc56f7156d9249c9b6
komalghai <komal.3ginfo@gmail.com>
2016-02-12 17:16:39 +0530

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -21,6 +21,46 @@ if(!empty($action)){
 				}
 			}
 			break;
+
+		case 'sendTestSMS':
+			$store = $_REQUEST['store'];
+			$storeData = file_get_contents("https://{$store}/admin/shop.json");
+			echo "<pre>";
+			print_R(json_decode($storeData));
+			exit();
+			return;
+			$user = SMS_USERNAME;
+			$password = SMS_PASSWORD; 
+			$mobilenumbers = "919XXXXXXXXX"; 
+			$message = "test messgae";
+			$senderid = SENDER_ID;
+			$messagetype = MESSAGE_TYPE;
+			$DReports = DELIVERY_REPORT;
+			$url = "http://www.smscountry.com/SMSCwebservice_Bulk.aspx";
+			$message = urlencode($message);
+			$ch = curl_init();
+			if (!$ch){
+				die("Couldn't initialize a cURL handle");
+			}
+			$ret = curl_setopt($ch, CURLOPT_URL,$url);
+			curl_setopt ($ch, CURLOPT_POST, 1);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+			curl_setopt ($ch, CURLOPT_POSTFIELDS, "User={$user}&passwd={$password}&mobilenumber={$mobilenumbers}&message={$message}&sid={$senderid}&mtype={$messagetype}&DR={$DReports}");
+			$ret = curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			$curlresponse = curl_exec($ch);
+			if(curl_errno($ch))
+			echo 'curl error : '. curl_error($ch);
+			if (empty($ret)) {
+				die(curl_error($ch));
+				curl_close($ch);
+			} else {
+				$info = curl_getinfo($ch);
+				curl_close($ch);
+				echo $curlresponse;
+				//echo "Message Sent Succesfully" ;
+			}
+			break;
 			
 		default:
 			break;
