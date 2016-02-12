c239bd364a6bc1741ef2ebe8ad30dd955099329e
komalghai <komal.3ginfo@gmail.com>
2016-02-12 12:11:35 +0530

    Update conf.php

--- conf.php
+++ conf.php
@@ -1,8 +1,8 @@
 <?php 
 define('SHOPIFY_APP_API_KEY', '2f32ee6aa7b785c18281b4cf8fc26346'); 
 define('SHOPIFY_APP_SHARED_SECRET', '8e7e97c6b762aed851b3387f5a249844');
-$GLOBALS['base_url'] = $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'].'/';
-$GLOBALS['ajax_url'] = $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'].'/ajax.php';
+$GLOBALS['base_url'] = 'https://'.$_SERVER['HTTP_HOST'].'/';
+$GLOBALS['ajax_url'] = 'https://'.$_SERVER['HTTP_HOST'].'/ajax.php';
 
 /** DB connectivity **/
 $host = "host=ec2-54-197-247-170.compute-1.amazonaws.com"; 
