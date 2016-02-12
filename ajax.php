28fee8a0d44168cab7a136194d834115f3b7a568
komalghai <komal.3ginfo@gmail.com>
2016-02-12 12:54:01 +0530

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -5,16 +5,20 @@ $action = $_REQUEST['action'];
 if(!empty($action)){
 	switch($action){
 		case 'saveSMS':
-			$key = $_REQUEST['key'];
-			$value = $_REQUEST['value'];
+			$key = $_REQUEST['key']; 
+			$value = mysql_real_escape_string($_REQUEST['value']);
 			$store = $_REQUEST['store'];
 			if(!empty($key) && !empty($value)){
 				$config = pg_query($db, "SELECT data FROM configuration WHERE store = '{$store}'");
 				$config = pg_fetch_assoc($config);
 				$config = unserialize($config['data']);
-				echo "<pre>";
-				print_R($config);
-				exit();
+				$config['SMSHTML'][$key] = $value;
+				$config = serialize($config);
+				if(pg_query($db, "UPDATE configuration SET data = '{$config}' WHERE store = '{$store}'")){
+					exit('1');
+				} else {
+					exit('0');
+				}
 			}
 			break;
 			
