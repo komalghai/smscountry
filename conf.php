8c2e1d0c91d0716843d1b0c3e5136e9b2bfd5e47
komalghai <komal.3ginfo@gmail.com>
2016-02-12 12:09:21 +0530

    Update conf.php

--- conf.php
+++ conf.php
@@ -1,7 +1,10 @@
 <?php 
 define('SHOPIFY_APP_API_KEY', '2f32ee6aa7b785c18281b4cf8fc26346'); 
 define('SHOPIFY_APP_SHARED_SECRET', '8e7e97c6b762aed851b3387f5a249844');
+$GLOBALS['base_url'] = $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'].'/';
+$GLOBALS['ajax_url'] = $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'].'/ajax.php';
 
+/** DB connectivity **/
 $host = "host=ec2-54-197-247-170.compute-1.amazonaws.com"; 
 $port = "port=5432"; 
 $dbname = "dbname=daovncsi96sin3"; 
@@ -9,5 +12,4 @@ $credentials = "user=vrjlpasdpeblfz password=N3mICKo5DkmsMjntro7bSPoQQA";
 $db = pg_connect( "$host $port $dbname $credentials" ); 
 if(!$db){ 
 	echo "Error : Unable to open database\n"; 
-} 
-?>
+}
