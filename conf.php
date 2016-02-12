fa07686e8d37e2d893ec4a4a54602e3c1ad42b2e
komalghai <komal.3ginfo@gmail.com>
2016-02-12 17:08:19 +0530

    Update conf.php

--- conf.php
+++ conf.php
@@ -1,8 +1,18 @@
 <?php 
-define('SHOPIFY_APP_API_KEY', '2f32ee6aa7b785c18281b4cf8fc26346'); 
-define('SHOPIFY_APP_SHARED_SECRET', '8e7e97c6b762aed851b3387f5a249844');
+/** saving globals **/
 $GLOBALS['base_url'] = 'https://'.$_SERVER['HTTP_HOST'].'/';
 $GLOBALS['ajax_url'] = 'https://'.$_SERVER['HTTP_HOST'].'/ajax.php';
+
+/** shopify credentials **/
+define('SHOPIFY_APP_API_KEY', '2f32ee6aa7b785c18281b4cf8fc26346'); 
+define('SHOPIFY_APP_SHARED_SECRET', '8e7e97c6b762aed851b3387f5a249844');
+
+/** SMS country setup **/
+define('SMS_USERNAME', 'shopifyplugin');
+define('SMS_PASSWORD', 'Shopify4plug');
+define('SENDER_ID', 'SMSCNTRY');
+define('MESSAGE_TYPE', 'N');
+define('DELIVERY_REPORT', 'Y');
 
 /** DB connectivity **/
 $host = "host=ec2-54-197-247-170.compute-1.amazonaws.com"; 
