4fcc77edf3c7b5ef178675dcf755cbe412a68be2
komalghai <komal.3ginfo@gmail.com>
2016-02-13 14:03:48 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,2 +1,8 @@
 <?php 
-// silence is golden
+global $db;
+if(!function_exists('saveMessage')){
+	function saveMessage($message, $recipient_name, $recipient_number, $message_type, $status="delivered"){
+		/* $response = pg_query($db, "INSERT INTO messages values()"); */
+		return true;
+	}
+}
