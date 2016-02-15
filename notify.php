aa8f7aa61335c84bbc936c2b5d91702729c7ce2c
komalghai <komal.3ginfo@gmail.com>
2016-02-15 15:10:57 +0530

    Update notify.php

--- notify.php
+++ notify.php
@@ -19,40 +19,38 @@ if(!empty($action)){
 	
 	switch($action){
 		case 'customer_signup':
-			/* if(!empty($data->default_address->phone)){
+			if(!empty($data->default_address->phone)){
 				$recipient_name = $data->default_address->name;
 				$customerMessage = $config['SMSHTML']['CustomerCustomerSignup'];
-				$customVariables = array(
-						'[shop_name]' => $storeData->shop->name,
-						'[shop_domain]' => $storeData->shop->domain,
-						'[customer_count]' => '36',
-						'[customer_firstname]' => $data,
-						'[customer_lastname]' => 'Doe',
-						'[verification_code]' => '5623321',
-						'[customer_address]' => '1444 S. Alameda Street',
-						'[customer_postcode]' => '90021',
-						'[customer_city]' => 'Los Angeles',
-						'[customer_country]' => 'United States',
-						'[order_id]' => '10045',
-						'[order_total]' => '$982.00',
-						'[order_products_count]' => '3',
-						'[order_status]' => 'Pending',
-						'[order_old_status]' => 'Pending',
-						'[order_new_status]' => 'Fullfilled',
-						'[order_date]' => '26th Jan, 2016',
-						'[return_product_name]' => 'Nokia v.2',
-						'[return_reason]' => 'The seal of package was broken, looks like it was used before.',
-						'[customer_name]' => 'John Doe',
-						'[customer_email]' => 'johndoe342@mail.com',
-						'[customer_message]' => 'Hi, This message is regarding testing.',
-					);
-				sendMessage($customerMessage, $data->default_address->phone, $recipient_name, 'CustomerCustomerSignup');
+				if(!empty($customerMessage)){
+					$customVariables = array(
+							'[shop_name]' => $storeData->shop->name,
+							'[shop_domain]' => $storeData->shop->domain,
+							'[customer_firstname]' => $data->first_name,
+							'[customer_lastname]' => $data->last_name,
+						);
+					foreach($customVariables as $find => $replace){
+						$customerMessage = str_replace($find, $replace, $customerMessage);
+					}
+					sendMessage($customerMessage, $data->default_address->phone, $recipient_name, 'CustomerCustomerSignup');
+				}
 			}
 			if(!empty($storeData->shop->phone)){
 				$adminMessage = $config['SMSHTML']['AdminCustomerSignup'];
-				sendMessage($adminMessage, $storeData->shop->phone, $storeData->shop->shop_owner, 'AdminCustomerSignup');
-			} */
-			pg_query($db, "UPDATE debug SET value = '{$data}' WHERE key = 'customer_signup'");
+				if(!empty($adminMessage)){
+					$customVariables = array(
+							'[shop_name]' => $storeData->shop->name,
+							'[shop_domain]' => $storeData->shop->domain,
+							'[customer_firstname]' => $data->first_name,
+							'[customer_lastname]' => $data->last_name,
+						);
+					foreach($customVariables as $find => $replace){
+						$adminMessage = str_replace($find, $replace, $adminMessage);
+					}
+					sendMessage($adminMessage, $storeData->shop->phone, $storeData->shop->shop_owner, 'AdminCustomerSignup');
+				}
+			}
+			/* pg_query($db, "UPDATE debug SET value = '{$data}' WHERE key = 'customer_signup'"); */
 			break;
 		case 'order_created':
 			pg_query($db, "UPDATE debug SET value = '{$data}' WHERE key = 'order_placed'");
