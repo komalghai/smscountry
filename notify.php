17a32f3de7701cab71f40a83c43699c652d7d36b
komalghai <komal.3ginfo@gmail.com>
2016-02-15 16:03:18 +0530

    Update notify.php

--- notify.php
+++ notify.php
@@ -2,6 +2,10 @@
 require('conf.php');
 if(!session_id()) session_start();
 global $db;
+date_default_timezone_set('Asia/Kolkata');
+$now = date('Y-m-d H:i:s');
+$updated = "Updated: {$now}";
+
 $store = $_SESSION['store'];
 $action = isset($_REQUEST['action']) ? $_REQUEST['action'] : null;
 $config = pg_query($db, "SELECT data FROM configuration WHERE store = '{$store}'");
@@ -50,6 +54,12 @@ if(!empty($action)){
 					sendMessage($adminMessage, $storeData->shop->phone, $storeData->shop->shop_owner, 'AdminCustomerSignup');
 				}
 			}
+			$webhook = fopen('php://input' , 'rb'); 
+			while(!feof($webhook)){
+				$updated .= fread($webhook, 4096); 
+			} 
+			fclose($webhook);
+			pg_query($db, "UPDATE debug SET value = '{$updated}' WHERE key = 'updated'");
 			/* pg_query($db, "UPDATE debug SET value = '{$data}' WHERE key = 'customer_signup'"); */
 			break;
 		case 'order_created':
