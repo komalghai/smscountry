4fc6aedb8bd7a545e3cc95fcc4495ab4f51cd384
komalghai <komal.3ginfo@gmail.com>
2016-02-15 13:55:12 +0530

    Update notify.php

--- notify.php
+++ notify.php
@@ -9,9 +9,6 @@ $config = pg_fetch_assoc($config);
 $config = unserialize($config['data']);
 $access_token = $config['access_token'];
 $storeData = @file_get_contents("https://{$store}/admin/shop.json?access_token={$access_token}");
-echo "<pre>";
-print_r($storeData);
-die;
 if(!empty($action)){
 	$data = '';
 	$webhook = fopen('php://input' , 'rb'); 
@@ -25,11 +22,35 @@ if(!empty($action)){
 			/* if(!empty($data->default_address->phone)){
 				$recipient_name = $data->default_address->name;
 				$customerMessage = $config['SMSHTML']['CustomerCustomerSignup'];
+				$customVariables = array(
+						'[shop_name]' => $storeData->shop->name,
+						'[shop_domain]' => $storeData->shop->domain,
+						'[customer_count]' => '36',
+						'[customer_firstname]' => $data,
+						'[customer_lastname]' => 'Doe',
+						'[verification_code]' => '5623321',
+						'[customer_address]' => '1444 S. Alameda Street',
+						'[customer_postcode]' => '90021',
+						'[customer_city]' => 'Los Angeles',
+						'[customer_country]' => 'United States',
+						'[order_id]' => '10045',
+						'[order_total]' => '$982.00',
+						'[order_products_count]' => '3',
+						'[order_status]' => 'Pending',
+						'[order_old_status]' => 'Pending',
+						'[order_new_status]' => 'Fullfilled',
+						'[order_date]' => '26th Jan, 2016',
+						'[return_product_name]' => 'Nokia v.2',
+						'[return_reason]' => 'The seal of package was broken, looks like it was used before.',
+						'[customer_name]' => 'John Doe',
+						'[customer_email]' => 'johndoe342@mail.com',
+						'[customer_message]' => 'Hi, This message is regarding testing.',
+					);
 				sendMessage($customerMessage, $data->default_address->phone, $recipient_name, 'CustomerCustomerSignup');
 			}
-			if(!empty()){
+			if(!empty($storeData->shop->phone)){
 				$adminMessage = $config['SMSHTML']['AdminCustomerSignup'];
-				sendMessage($adminMessage, $mobilenumber, $recipient_name, 'AdminCustomerSignup');
+				sendMessage($adminMessage, $storeData->shop->phone, $storeData->shop->shop_owner, 'AdminCustomerSignup');
 			} */
 			pg_query($db, "UPDATE debug SET value = '{$data}' WHERE key = 'customer_signup'");
 			break;
