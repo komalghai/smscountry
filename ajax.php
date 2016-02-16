d1a69afe4014e46a3793a7cdf32ff645a8799a95
komalghai <komal.3ginfo@gmail.com>
2016-02-16 12:59:31 +0530

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -6,7 +6,7 @@ if(!empty($action)){
 	switch($action){
 		case 'saveSMS':
 			$key = $_REQUEST['key']; 
-			$value = $_REQUEST['value'];
+			$value = urlencode($_REQUEST['value']);
 			$store = $_REQUEST['store'];
 			if(!empty($key) && !empty($value)){
 				$config = pg_query($db, "SELECT data FROM configuration WHERE store = '{$store}'");
