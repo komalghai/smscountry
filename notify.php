dbbb1fc2a839ed384b843b73c9c306c7b8a6620f
komalghai <komal.3ginfo@gmail.com>
2016-02-16 16:48:12 +0530

    Update notify.php

--- notify.php
+++ notify.php
@@ -6,15 +6,15 @@ global $db;
 /* $updated = print_r($_REQUEST, true); */
 /* pg_query($db, "UPDATE debug SET value = '{$updated}' WHERE key = 'updated'"); */
 $action = explode('::', $_REQUEST['action']);
-echo "<pre>";
-print_R($action);
-die;
 $action = $action[0];
 $store = $action[1];
 $config = pg_query($db, "SELECT data FROM configuration WHERE store = '{$store}'");
 echo "SELECT data FROM configuration WHERE store = '{$store}'";
 $config = pg_fetch_assoc($config);
 $config = unserialize($config['data']);
+echo "<pre>";
+print_R($action);
+die;
 $access_token = $config['access_token'];
 $storeData = @file_get_contents("https://{$store}/admin/shop.json?access_token={$access_token}");
 if(!empty($action)){
