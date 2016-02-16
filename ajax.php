2506efaab29971abbc626d91d598b4ae2bbf3a79
komalghai <komal.3ginfo@gmail.com>
2016-02-16 17:29:46 +0530

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -13,10 +13,10 @@ if(!empty($action)){
 				$config = pg_fetch_assoc($config);
 				$config = unserialize($config['data']);
 				$config['SMSHTML'][$key] = $value;
+				$config = serialize($config);
 				echo "<pre>";
 				print_r($config);
 				die;
-				$config = serialize($config);
 				$updated = pg_query($db, "UPDATE configuration SET data = '{$config}' WHERE store = '{$store}'");
 				if($updated){
 					exit('1');
