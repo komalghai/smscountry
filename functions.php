5b593f47c914525cb8f6733854b9bda1a0e5e111
komalghai <komal.3ginfo@gmail.com>
2016-02-16 18:18:08 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -4,7 +4,8 @@ if(!function_exists('saveMessage')){
 		global $db;
 		$lastRow = pg_query($db, "SELECT id FROM messages ORDER by id DESC limit 1");
 		$lastID = pg_fetch_assoc($lastRow);
-		$lastID = (pg_num_rows($lastRow) > 0) ? $lastID['id'] : 1;
+		$lastID = (pg_num_rows($lastRow) > 0) ? $lastID['id'] : 0;
+		$lastID = $lastID + 1;
 		$character_count = strlen($message);
 		$created_at = date('Y-m-d H:i:s');
 		return pg_query($db, "INSERT INTO messages (id, message_text, recipient_name, recipient_number, type, character_count, created_at, status) VALUES ('{$lastID}', '{$message}', '{$recipient_name}', '{$recipient_number}', '{$message_type}', '{$character_count}', '{$created_at}', '{$status}')");
