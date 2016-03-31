6f2bf7a19d7d62f6e277642949942fa420e42fda
komalghai <komal.3ginfo@gmail.com>
2016-03-31 18:42:32 +0530

    Update settings.php

--- settings.php
+++ settings.php
@@ -33,6 +33,16 @@ $historyData = pg_query($db, "SELECT * FROM messages ORDER BY id DESC");
 $config= pg_query($db, "SELECT * FROM debug where id =5");
 $config = pg_fetch_assoc($config);
 //echo '<pre>hello';print_r($config); echo'</pre>';
+if(isset($_REQUEST['smssubmit'])){
+	$lastRow = pg_query($db, "SELECT id FROM smscountrydetail ORDER by id DESC limit 1");
+		$lastID = pg_fetch_assoc($lastRow);
+		$lastID = (pg_num_rows($lastRow) > 0) ? $lastID['id'] : 0;
+		$lastID = $lastID + 1;
+	$SMS_USERNAME=$_REQUEST['SMS_USERNAME'];
+	$SMS_PASSWORD=$_REQUEST['SMS_PASSWORD'];
+	$SENDER_ID=$_REQUEST['SENDER_ID'];
+pg_query($db, "INSERT INTO smscountrydetail (id, SMS_USERNAME, SMS_PASSWORD, SENDER_ID) VALUES ('{$lastID}','{$SMS_USERNAME}', '{$SMS_PASSWORD}', '{$SENDER_ID}')");
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -244,6 +254,13 @@ $config = pg_fetch_assoc($config);
 	<body>
 		<div class="col-xs-12 padding-top sms-config">
 			<h3 class="alert alert-info">Configuration</h3>
+			<h4>Sms Country Detail</h4>
+			<form name="smscountryd" class="">
+			SMS USERNAME<input type="text" name="SMS_USERNAME">
+			SMS PASSWORD<input type="text" name="SMS_PASSWORD">
+			SENDER ID<input type="text" name="SENDER_ID">
+			<input type="submit" class="btn" name="smssubmit" />
+			</form>
 			<form>
 				<div class="box container well col-xs-12" style="padding: 0">
 					<ul class="tabs nav">
