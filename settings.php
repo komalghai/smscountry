3b909ec7c08b1880d6fb6b6588b3df8e5fddb0fd
komalghai <komal.3ginfo@gmail.com>
2016-04-01 16:45:27 +0530

    Update settings.php

--- settings.php
+++ settings.php
@@ -571,7 +571,7 @@ $sender_id=$config['sender_id'];
 					<h5>filter By </h5>
 					Phone no <select name="phonefilter">
 					<?php $recipient_number1 = pg_query($db, "SELECT DISTINCT recipient_number FROM messages ORDER BY id DESC"); 
-							while($recipient_number = pg_fetch_assoc($recipient_number)) {
+							while($recipient_number = pg_fetch_assoc($recipient_number1)) {
 								echo "<option value=".$recipient_number1['recipient_number'].">".$recipient_number1['recipient_number']."</option>";
 								
 							}
